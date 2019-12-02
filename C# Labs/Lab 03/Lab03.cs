d5e08a12934d840ce56771eb9ec2ae1f5e96f544
Yohannes Habte <yohannes.habte@mnsu.edu>
2019-12-02 07:44:35 -0600

    Add files via upload
    cs file upload

---
+++ C# Labs/Lab 03/Lab03.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs
+{
+    class Lab03
+    {
+        static void Main(string[] args)
+        {
+            double curBal = 45.32;
+
+            double amount;
+            Console.Write("Please enter a amount to update account by $");
+            amount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Customer's balance (before transaction) = $"+curBal);
+            Console.WriteLine("Requested update amount = $"+amount);
+
+            double actAmount;
+            actAmount = transaction(curBal, amount);
+
+            curBal += actAmount;
+
+            Console.WriteLine("Actual update amount = $" + actAmount);
+            Console.WriteLine("Customer's balance (after transaction) = $" + curBal);
+
+            Console.WriteLine("\nThank you and good-bye!\n");
+        }
+        static Double transaction(Double balance, Double amount)
+        {
+            if(amount >= 0)
+                return amount;
+            if (balance < -amount)
+                return 0.0;
+            return amount;
+        }
+    }
+}
